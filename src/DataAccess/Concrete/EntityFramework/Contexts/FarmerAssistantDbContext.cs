47ec313892e5d0c54685ade38a5ad3c3129e25da
Ömer Faruk ZIRHOĞLU <o.f.zirhoglu@gmail.com>
2021-05-29 16:07:05 +0300

    Added FarmerAssistantDbContext

---
+++ src/DataAccess/Concrete/EntityFramework/Contexts/FarmerAssistantDbContext.cs
@@ -0,0 +1,20 @@
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class FarmerAssistantDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=FarmerAssistantDB;Trusted_Connection=true");
+        }
+
+        public DbSet<Field> Fields { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Harvest> Harvests { get; set; }
+        public DbSet<HarvestType> HarvestTypes { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleType> SaleTypes { get; set; }
+    }
+}
