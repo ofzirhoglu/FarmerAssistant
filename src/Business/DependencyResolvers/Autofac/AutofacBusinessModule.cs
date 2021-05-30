72133d85bafecdf61541178287230a1151dbb013
Ömer Faruk ZIRHOĞLU <o.f.zirhoglu@gmail.com>
2021-05-30 20:36:58 +0300

    Configured Interceptors infrastructure with Autofac

--- src/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ src/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -12,6 +15,16 @@ namespace Business.DependencyResolvers.Autofac
         {
             builder.RegisterType<FieldManager>().As<IFieldService>().SingleInstance();
             builder.RegisterType<EfFieldDal>().As<IFieldDal>().SingleInstance();
+
+            // Çalışan uygulama içerisinde ...
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            // ... Implemente edilmiş interface leri bul ...
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    // ... ve onlar için AspectInterceptorSelector ı çağır
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
