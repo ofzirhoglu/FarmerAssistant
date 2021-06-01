a279c4a75ce00daa81ecfedac58a5fbcd12b5629
Ömer Faruk ZIRHOĞLU <o.f.zirhoglu@gmail.com>
2021-06-01 17:51:56 +0300

    Added UserForLoginDto and UserForRegisterDto

---
+++ src/Entities/DTOs/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+using Core.Entities.Abstract;
+
+namespace Entities.DTOs
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
