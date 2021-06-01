c4d6a867a094330eda32f0463c24395842be89b3
Ömer Faruk ZIRHOĞLU <o.f.zirhoglu@gmail.com>
2021-06-01 17:58:40 +0300

    Added IAuthService, AuthManager - Edited Messages

--- src/Business/Constants/Messages.cs
+++ src/Business/Constants/Messages.cs
@@ -5,15 +5,17 @@ namespace Business.Constants
     public static class Messages
     {
         public static string FieldAdded = "Tarla eklendi";
-
         public static string FieldDeleted = "Tarla silindi";
         public static string FieldUpdated = "Tarla güncellendi";
-
         public static string FieldsListed = "Tarlalar listelendi";
         public static string FieldGetById = "Tarla Id'ye göre getirildi";
-
         public static string FieldNameAlreadyExists = "Bu isimde zaten başka bir tarla var";
-
         public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten var";
+        public static string AccessTokenCreated = "Token oluşturuldu";
+        public static string SuccessfulLogin = "Giriş başarılı";
     }
 }
