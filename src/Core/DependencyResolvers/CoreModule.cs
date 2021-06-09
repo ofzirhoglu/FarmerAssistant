6c11fc96f9fa9f0139c9d4e6b0b556630010ad86
Ömer Faruk ZIRHOĞLU <o.f.zirhoglu@gmail.com>
2021-06-09 15:27:12 +0300

    Added MemoryCache operations

--- src/Core/DependencyResolvers/CoreModule.cs
+++ src/Core/DependencyResolvers/CoreModule.cs
@@ -1,3 +1,5 @@
+using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,9 +10,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
 
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
         }
     }
 }
--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -92,7 +92,7 @@ namespace WebApi
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Farmer Assistant v1");
-                    c.RoutePrefix = string.Empty;
+                    //c.RoutePrefix = string.Empty;
                 });
             }
 
